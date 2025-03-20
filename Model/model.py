2bcd6b89f0b32a079927b9a1d1722f4af369753b
Gabriel Matioli <gabematioli@outlook.com>
2025-03-19 17:15:56 -0700

    Reset

--- Model/model.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-twitter_df = pd.read_csv('../Data/Clean_Twitter_Data.csv')
-X, y = twitter_df.drop(columns=['category']).copy(), twitter_df['category'].copy()
