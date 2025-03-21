9442cfcf5ada7ab25c9267bb22ac6316c6ee86c4
Gabriel Matioli <gabematioli@outlook.com>
2025-03-21 14:55:50 -0700

    Model updates

--- Model/model.py
+++ Model/model.py
@@ -1,22 +1,29 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
+from sklearn.metrics import classification_report
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+def sentiment_analysis_model(x, y, return_clf=True, test_size=0.20, seed=42,):
+
+    clf = MultinomialNB()
+    
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=test_size, random_state=seed)
+
+    clf.fit(X_train, y_train)
+
+    y_prediction = clf.predict(X_test)
+
+    clf_report = classification_report(y_test, y_prediction)
+
+    print('Sentiment Analysis Report: {clf_report}')
+
+    if return_clf:
+        return clf
 
 reddit_df = pd.read_csv('../Data/Clean_Reddit_Data.csv')
 x, y = reddit_df.drop(columns=['category']).copy(), reddit_df['category'].copy()
 
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.15, random_state=42)
+clf = sentiment_analysis_model(x, y)
 
-clf = MultinomialNB()
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-
-clf_report = classification_report(y_test, y_pred)
-print(clf_report)
-
-cm = confusion_matrix(y_test, y_pred)
-ConfusionMatrixDisplay(cm, display_labels=clf.classes_).plot()
-plt.show()
