7d0a94166b690e8f8dec57b9b50d1dd094373d3a
Gabriel Matioli <gabematioli@outlook.com>
2025-03-22 10:12:26 -0700

    Added error checking to model

--- Model/model.py
+++ Model/model.py
@@ -5,20 +5,38 @@ from sklearn.metrics import classification_report
 
 def sentiment_analysis_model(x, y, return_clf=True, test_size=0.20, seed=42, alpha=1):
 
-    clf = MultinomialNB(alpha=alpha)
+    if not isinstance(return_clf, bool):
+        raise ValueError('return_clf is not an instance of bool')
     
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=test_size, random_state=seed)
+    elif not isinstance(test_size, (float, int)) or not (0 < test_size <= 1):
+        raise ValueError('test_size must be a float or int between 0 and 1')
+    
+    elif not isinstance(seed, (float, int)) or not (seed >= 0):
+        raise ValueError('seed must be a non-negative float or int')
+    
+    elif not isinstance(alpha, (float, int)) or not (alpha >= 0):
+        raise ValueError('alpha must be non-negative float or int')
 
-    clf.fit(X_train, y_train)
+    try:
+        
+        clf = MultinomialNB(alpha=alpha)
+        
+        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=test_size, random_state=seed)
 
-    y_prediction = clf.predict(X_test)
+        clf.fit(X_train, y_train)
 
-    clf_report = classification_report(y_test, y_prediction)
+        y_prediction = clf.predict(X_test)
 
-    print(f'Sentiment Analysis Report: {clf_report}')
+        clf_report = classification_report(y_test, y_prediction)
 
-    if return_clf:
-        return clf
+        print('Sentiment Analysis Report:\n')
+        print(clf_report)
+
+        if return_clf:
+            return clf
+        
+    except Exception as e:
+        raise RuntimeError(f'Error occurred: {e}')
 
 reddit_df = pd.read_csv('../Data/Clean_Reddit_Data.csv')
 x, y = reddit_df.drop(columns=['category']).copy(), reddit_df['category'].copy()
